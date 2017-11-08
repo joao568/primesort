a8e7909c6f7b95ffd8d9dbbe51838a3990663b98
joao568 <jguilherme.marcos@gmail.com>
2017-11-08 10:37:05 -0200

    codigo pronto

--- src/main.c
+++ src/main.c
@@ -1,17 +1,49 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
-int main() {
+int EhPrimo(int n){
+    int ehPrimo=1, d=2;
+    if(n==1){
+        return 0;
+    }
+    if(n<=1){
+        ehPrimo=0;
+    }
+    while(ehPrimo==1 && d<=n/2){
+        if(n%d==0){
+            ehPrimo=0;
+        }
+        d=d+1;
+    }
+}
 
-  int x, y;
+void ordena (int vet[], int n) {
+    int k, j, aux;
+    for (k = 1; k < n; k++) {
+        for (j = 0; j < n - k; j++) {
+            if (vet[j] < vet[j + 1]) {
+                aux          = vet[j];
+                vet[j]     = vet[j + 1];
+                vet[j + 1] = aux;
+            }
+        }
+    }
+}
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+int main(){
+    int n=0, ehPrimo=0, i=0, vetor[100], p;
+    while(n!=-1){
+        scanf("%d", &n);
+        ehPrimo=EhPrimo(n);
+        if(ehPrimo==0){
+            vetor[i]=n;
+            i++;
+        }
+    }
+    vetor[i]=-1;
+    ordena(vetor,i);
+    for(p=0;p<i;p++){
+        printf("%d\n", vetor[p]);
+    }
+
+return 0;
 }
